34f0eb90c148de2774802f451860b728e857ee2f
ALUNO <ALUNO@10.85.198.7>
2024-06-06 15:45:36 -0300

    Commit project InfiniteLoop

---
+++ src/InfiniteLoop.java
@@ -0,0 +1,9 @@
+public class InfiniteLoop {
+    public static void main(String[] args) {
+   
+    // The for loop will run infinite times.
+    for(int i = 5; i >= 1; i-- ){
+        System.out.println("Hello");
+    }
+  }
+}
